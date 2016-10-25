337c9842513b1199f102779f79321b1c6c4cffdb
msci <mayurb@softcrylic.co.in>
2016-10-25 14:57:00 +0530

    updating plugin version

--- src/main/java/in/mayurshah/TestMGMakerMojo.java
+++ src/main/java/in/mayurshah/TestMGMakerMojo.java
@@ -1,5 +1,6 @@
 package in.mayurshah;
 
+import in.mayurshah.util.Const;
 import in.mayurshah.util.EnvVarToFile;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -13,6 +14,21 @@ import org.apache.maven.plugins.annotations.Mojo;
 public class TestMGMakerMojo extends AbstractMojo{
     public void execute() throws MojoExecutionException, MojoFailureException {
         EnvVarToFile envVarToFile = new EnvVarToFile();
+        verifyEnvVariableExists(Const.suiteName);
+        verifyEnvVariableExists(Const.ReportLocation);
+        verifyEnvVariableExists(Const.remoteURL);
+        verifyEnvVariableExists(Const.baseURL);
+        verifyEnvVariableExists(Const.internal);
+        verifyEnvVariableExists(Const.parallel);
+        verifyEnvVariableExists(Const.thread_count);
+        verifyEnvVariableExists(Const.tests);
+        verifyEnvVariableExists(Const.browsers);
+        verifyEnvVariableExists(Const.packageName);
         envVarToFile.write();
+    }
+    public void verifyEnvVariableExists(String var){
+        if(System.getenv(var) == null || "".equals(System.getenv(var)))
+            throw new NullPointerException("Env " + var + " is mandatory.");
+
     }
 }
